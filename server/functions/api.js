71cd430add82cd7fdafc60437fe21f7cb772279a
Annas03 <baigannas9@gmail.com>
2023-02-10 19:49:10 +0500

    config

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const {createImage, shareImage, getImage} = require('../controller/tasks')
+const {createImage, shareImage, getImage} = require('../functions/controller/tasks')
 const serverless = require('serverless-http')
-const connectDB = require('../db/connect')
+const dbConfig = require("../functions/db/connect");
 const cors = require('cors')
 const app = express()
 PORT = process.env.PORT || 5000
@@ -9,24 +9,31 @@ PORT = process.env.PORT || 5000
 app.use(express.json({limit: '50mb'}));
 app.use(cors())
 
-app.post("/.netlify/functions/api/post", createImage)
-app.post("/.netlify/functions/api/share", shareImage)
-app.get("/.netlify/functions/api/get", getImage)
-app.get("/.netlify/functions/api/", (req, res) => {
+app.post("/api/post", createImage)
+app.post("/api/share", shareImage)
+app.get("/api/get", getImage)
+app.get("/api/", (req, res) => {
     res.send("Server is Running...")
 })
 
-const start = async () => {
-    try {
-        await connectDB()
-        app.listen(PORT, ()=>{
-            console.log("server is listning...")
-        })
-    } catch (error) {
-        console.log("Error in Connecting to DB")
-    }
-}
+app.listen(PORT, ()=>{
+        console.log("server is listning...")
+    })
 
-module.exports.handler = serverless(app)
+// const start = async () => {
+//     try {
+//         await connectDB()
+//         app.listen(PORT, ()=>{
+//             console.log("server is listning...")
+//         })
+//     } catch (error) {
+//         console.log("Error in Connecting to DB")
+//     }
+// }
 
 // start()
+// const handler = serverless(app);
+// module.exports.handler = async (event, context) => {
+//   const result = await handler(event, context);
+//   return result;
+// };
